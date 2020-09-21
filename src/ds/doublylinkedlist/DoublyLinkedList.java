614f669abcaeb161985338e9caec9de7e6b88afe
Parth <parthpuri2000@gmail.com>
2020-09-21 19:20:20 +0530

    updating doubly linked list by adding delete functionality

--- src/ds/doublylinkedlist/DoublyLinkedList.java
+++ src/ds/doublylinkedlist/DoublyLinkedList.java
@@ -101,5 +101,55 @@ public class DoublyLinkedList {
 		return true;
 	}
 	
+	//assume non-empty list
+	
+	public Node deleteKey(int key) {
+		Node current = first;
+		
+		while(current.data!=key) {
+			current = current.next;
+			if(current==null)
+				return null;
+		}
+		
+		
+		if(current==first) {
+			first = current.next;
+		} else {
+			current.previous.next = current.next;
+		}
+		
+		if(current==last) {
+			last = current.previous;
+		} else {
+			current.next.previous = current.previous;
+		}
+		
+		return current;
+	
+	}
+	
+
+		public void displayForward() {
+			System.out.print("List (first --> last) ");
+			
+			Node current = first;
+			while(current!=null) {
+				current.displayNode();
+				current = current.next;
+			}
+		}
+
+		public void displayBackward() {
+			System.out.print("List (last --> first) ");
+			
+			Node current = last;
+			while(current!=null) {
+				current.displayNode();
+				current = current.previous;
+			}
+		}
+	
+	
 		
 }
